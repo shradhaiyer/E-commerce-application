3d978b3a053d9be80ec16f9be857dd3daf2101dd
shradhaiyer <66390588+shradhaiyer@users.noreply.github.com>
2021-08-04 23:00:05 +0530

    Filter by category and Prices

--- client/src/core/RadioBox.js
+++ client/src/core/RadioBox.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect, Fragment } from 'react'
-import { prices } from './fixedPrices';
 
-const RadioBox = () => {
+const RadioBox = ({prices, handleFilters}) => {
     const [value, setValue] = useState(0);
 
-    const handleChange = () => {
-
+    const handleChange = (event) => {
+        handleFilters(event.target.value);
+        setValue(event.target.value);
+        console.log(value)
     }
     return prices.map((p,i) => {
-        return <div>
+        return <div key={i}>
             <input type="radio" 
             className="mr-2 ml-4"
             value={`${p._id}`}
-            onChange={handleChange()}/>
+            name={p}
+            onChange={handleChange}/>
             <label className="form-check-label">{p.name}</label>
         </div>
     })
--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,64 +1,144 @@
-import React, {useEffect, useState} from 'react';
-import Layout from './Layout';
-import { getProducts } from './apiCore';
-import Checkbox from './Checkbox';
-import RadioBox from './RadioBox';
-import { getCategories } from './apiCore';
-import { prices } from './fixedPrices';
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import Card from "./Card";
+import { getCategories, getFilteredProducts } from "./apiCore";
+import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
 
 const Shop = () => {
-    const [categories, setCategories] = useState([]);
     const [myFilters, setMyFilters] = useState({
-        filters: {
-            category:[],
-            price: []
-        }
-    });    
+        filters: { category: [], price: [] }
+    });
+    const [categories, setCategories] = useState([]);
     const [error, setError] = useState(false);
+    const [limit, setLimit] = useState(1);
+    const [skip, setSkip] = useState(0);
+    const [size, setSize] = useState(0);
+    const [filteredResults, setFilteredResults] = useState([]);
 
-    const loadCategories = () =>  {
+    const init = () => {
         getCategories().then(data => {
-            if(data.err) {
-                setError(data.err);
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setCategories(data);
             }
-            else {
-              setCategories(data)
-            }
-        }
-        );
-    }
+        });
+    };
 
-    const handleFilters = (filters, filterBy) => {
-        const newFilters = {...myFilters};
-        newFilters.filters[filterBy] = filters;
-        setMyFilters(newFilters);        
-    }
+    const loadFilteredResults = newFilters => {
+        // console.log(newFilters);
+        getFilteredProducts(skip, limit, newFilters).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setFilteredResults(data.data);
+                setSize(data.size);
+                setSkip(0);
+            }
+        });
+    };
+
+    const loadMore = () => {
+        let toSkip = skip + limit;
+        // console.log(newFilters);
+        getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setFilteredResults([...filteredResults, ...data.data]);
+                setSize(data.size);
+                setSkip(toSkip);
+            }
+        });
+    };
+
+    const loadMoreButton = () => {
+        return (
+            size > 0 &&
+            size >= limit && (
+                <button onClick={loadMore} className="btn btn-warning mb-5">
+                    Load more
+                </button>
+            )
+        );
+    };
 
     useEffect(() => {
-        loadCategories();
-    },[]);
+        init();
+        loadFilteredResults(skip, limit, myFilters.filters);
+    }, []);
+
+    const handleFilters = (filters, filterBy) => {
+        // console.log("SHOP", filters, filterBy);
+        const newFilters = { ...myFilters };
+        newFilters.filters[filterBy] = filters;
+
+        if (filterBy === "price") {
+            let priceValues = handlePrice(filters);
+            newFilters.filters[filterBy] = priceValues;
+        }
+        loadFilteredResults(myFilters.filters);
+        setMyFilters(newFilters);
+    };
+
+    const handlePrice = value => {
+        const data = prices;
+        let array = [];
+
+        for (let key in data) {
+            if (data[key]._id === parseInt(value)) {
+                array = data[key].array;
+            }
+        }
+        return array;
+    };
 
     return (
-        <Layout title="Shop" description="Shop for your favourite books" className="container-fluid">
-            
-            <div className ="row">
+        <Layout
+            title="Shop Page"
+            description="Search and find books of your choice"
+            className="container-fluid"
+        >
+            <div className="row">
                 <div className="col-4">
                     <h4>Filter by categories</h4>
-                    <ul><Checkbox categories={categories}
-                         handleFilters={filters => handleFilters(filters, 'category')}/></ul>   
-                    <h4>Filter by Price</h4>
-                    <div><RadioBox/></div>   
+                    <ul>
+                        <Checkbox
+                            categories={categories}
+                            handleFilters={filters =>
+                                handleFilters(filters, "category")
+                            }
+                        />
+                    </ul>
+
+                    <h4>Filter by price range</h4>
+                    <div>
+                        <RadioBox
+                            prices={prices}
+                            handleFilters={filters =>
+                                handleFilters(filters, "price")
+                            }
+                        />
+                    </div>
                 </div>
-               
+
                 <div className="col-8">
-                    {JSON.stringify(myFilters)}
+                    <h2 className="mb-4">Products</h2>
+                    <div className="row">
+                        {filteredResults.map((product, i) => (
+                            <div key={i}>
+                                <Card product={product} />
+                            </div>
+                        ))}
+                    </div>
+                    <hr />
+                    {loadMoreButton()}
                 </div>
             </div>
-            
-            
         </Layout>
-    )
-}
+    );
+};
 
 export default Shop;
